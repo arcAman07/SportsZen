ead315152df2c4fe8e311772ab89407c8648ef51
Aman <aman.sharma2020b@vitstudent.ac.in>
2021-11-21 23:04:42 +0530

    server.js updated

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const fs = require("fs");
 const path = require("path");
+const Web3 = require("web3");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
